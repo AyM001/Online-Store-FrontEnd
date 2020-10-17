a09d3bb7872381083daae6c5f415e0d10a38f929
AyM001 <aymaaan001@gmail.com>
2020-10-17 12:04:24 +0300

    product list

--- onlineStoreF/src/app/app.module.ts
+++ onlineStoreF/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
+import {ProductServiceService} from './products/service/product-service.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule
   ],
-  providers: [],
+  providers: [ProductServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
